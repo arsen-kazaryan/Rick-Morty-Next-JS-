8a658006f1a8da1a209bea2e64689b62b2e43113
Arsen Kazaryan <arsenk001@mail.ru>
2026-07-16 14:21:21 +0300

    refactor: rename getpage to getCharactersPage, drop unused getCharacters

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 import { ApiResponse, Character, LocationItem } from "@/types";
+import { notFound } from "next/navigation";
 
 export const BASE_URL = "https://rickandmortyapi.com/api";
 
@@ -6,18 +7,12 @@ async function fetchJson<T>(url: string): Promise<T> {
   const res = await fetch(url, {
     next: { revalidate: 120 },
   });
-
+  if(res.status === 404) notFound()
   if (!res.ok) throw new Error(`Failed to fetch: ${url}`);
 
   return res.json();
 }
 
-export async function getCharacters(): Promise<Character[]> {
-  const data = await fetchJson<{ results: Character[] }>(
-    `${BASE_URL}/character`
-  );
-  return data.results;
-}
 
 
 export async function getCharacter(id: string): Promise<Character> {
@@ -26,10 +21,10 @@ export async function getCharacter(id: string): Promise<Character> {
 
 
 
-export async function getpage(id: string): Promise<Character[]> {
-  const data = await fetchJson<ApiResponse>(`${BASE_URL}/character/?page=${id}`);
-  return data.results;
+export async function getpage(page: string): Promise<ApiResponse> {
+  return fetchJson<ApiResponse>(`${BASE_URL}/character/?page=${page}`);
 }
+
 export async function getLocations(): Promise<LocationItem[]> {
   const data = await fetchJson<{ results: LocationItem[] }>(
     `${BASE_URL}/location`
